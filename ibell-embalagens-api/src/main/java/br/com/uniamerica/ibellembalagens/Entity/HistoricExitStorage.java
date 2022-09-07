7fc0aeef9f173c8281d2691e9f709a49afc1d926
renan montano paz <renanmontanopaz@outlook.com>
2022-09-07 17:05:32 -0300

    atualizando as classes

--- ibell-embalagens-api/src/main/java/br/com/uniamerica/ibellembalagens/Entity/HistoricExitStorage.java
+++ ibell-embalagens-api/src/main/java/br/com/uniamerica/ibellembalagens/Entity/HistoricExitStorage.java
@@ -2,5 +2,14 @@ package br.com.uniamerica.ibellembalagens.Entity;
 
 public class HistoricExitStorage extends AbstractEntity {
     private String Historic;
+    private Client client;
+    private Product product;
 
+    public String getHistoric() {
+        return Historic;
+    }
+
+    public void setHistoric(String historic) {
+        Historic = historic;
+    }
 }
