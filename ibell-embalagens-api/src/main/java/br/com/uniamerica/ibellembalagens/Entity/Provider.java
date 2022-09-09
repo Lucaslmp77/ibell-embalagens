0bf0f5d8895c9358e0126b65f451e7a88919eea4
JoaoSchulz <joaoguilherme.da@gmail.com>
2022-09-09 14:15:09 -0300

    Lombok

--- ibell-embalagens-api/src/main/java/br/com/uniamerica/ibellembalagens/Entity/Provider.java
+++ ibell-embalagens-api/src/main/java/br/com/uniamerica/ibellembalagens/Entity/Provider.java
@@ -1,90 +1,35 @@
 package br.com.uniamerica.ibellembalagens.Entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
 import java.time.LocalDateTime;
 
+@Entity
 public class Provider extends AbstractEntity{
+    @Getter
+    @Setter
     private String name;
+    @Getter
+    @Setter
     private String cnpjCpf;
+    @Getter
+    @Setter
     private String phoneNumber;
+    @Getter
+    @Setter
     private String addres;
+    @Getter
+    @Setter
     private String email;
+    @Getter
+    @Setter
     private LocalDateTime dateRegister;
+    @Getter
+    @Setter
     public String observation;
 
-    public Provider(String name, String cnpjCpf, String phoneNumber, String addres, String email, LocalDateTime dateRegister){
-        this.name = name;
-        this.cnpjCpf = cnpjCpf;
-        this.phoneNumber = phoneNumber;
-        this.addres = addres;
-        this.email = email;
-        this.dateRegister = dateRegister;
-    }
 
-    public Provider(){}
-
-    public Provider(String name, String cnpjCpf, String phoneNumber, String addres, String email, LocalDateTime dateRegister, String observation){
-        this.name = name;
-        this.cnpjCpf = cnpjCpf;
-        this.phoneNumber = phoneNumber;
-        this.addres = addres;
-        this.email = email;
-        this.dateRegister = dateRegister;
-        this.observation = observation;
-    }
-
-    public String getName(){
-        return this.name;
-    }
-    public void setName(String name){
-        this.name= name;
-    }
-
-    public String getCnpjCpf(){
-        return this.cnpjCpf;
-    }
-    public void setCnpjCpf(String cnpjCpf){
-        this.cnpjCpf = cnpjCpf;
-    }
-
-    public String getPhoneNumber(){
-        return this.phoneNumber;
-    }
-    public void setPhoneNumber(String phoneNumber){
-        this.phoneNumber = phoneNumber;
-    }
-
-    public String getAddres(){
-        return this.addres;
-    }
-    public void setAddres(String addres){
-        this.addres = addres;
-    }
-
-    public String getEmail(){
-        return this.email;
-    }
-    public void setEmail(String email){
-        this.email = email;
-    }
-
-    public LocalDateTime getDateRegister(){
-        return this.dateRegister;
-    }
-    public void setDateRegister(LocalDateTime dateRegister){
-        this.dateRegister = dateRegister;
-    }
-
-    @Override
-    public String toString() {
-        return "Provider{" +
-                "name='" + name + '\'' +
-                ", cnpjCpf='" + cnpjCpf + '\'' +
-                ", phoneNumber='" + phoneNumber + '\'' +
-                ", addres='" + addres + '\'' +
-                ", email='" + email + '\'' +
-                ", dateRegister=" + dateRegister +
-                ", observation='" + observation + '\'' +
-                '}';
-    }
 
 }
